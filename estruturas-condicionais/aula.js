74029545c21f990dd35e8a133ff088af9ff09ceb
Joice Cristina <joice1509oliveira@gmail.com>
2023-07-05 17:16:19 -0300

    começo da segunda aula

---
+++ estruturas-condicionais/aula.js
@@ -0,0 +1,23 @@
+// const numero = 2;
+
+// const numeroPar = (numero % 2) === 0;
+
+// if (numeroPar) {
+//     console.log('Par');
+// } else {
+//     console.log('Ímpar');
+// }
+
+//----------------------------
+
+const numero2 = 2;
+
+const numeroDivisivelPor5 = (numero2 % 5) === 0;
+
+if (numero2 === 0) {
+    console.log('O número é inválido');
+} else if (numeroDivisivelPor5) {
+    console.log('O número é divisivel por 5');
+} else {
+    console.log('O número não é divisivel por 5');
+}
---
+++ estruturas-condicionais/segundo-desafio.js
@@ -0,0 +1,14 @@
+/*
+Faça um programa para calcular o valor de uma viagem.
+
+Você terá 5 variáveis. Sendo elas:
+1- Preço do etanol
+2- Preço da gasolina
+3- O tipo de combustivel que está no seu carro
+4- Gasto médio de combustível do carro por Km
+5- Distância em Km da viagem
+
+Imprima no console o valor que será gasto para realizar esta viagem
+*/ 
+
+// Vídeo parou em 30 minutos
