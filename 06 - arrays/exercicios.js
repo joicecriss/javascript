3cacb572f64ce76227d46a40e24b2ed3c4893ede
Joice Cristina <joice1509oliveira@gmail.com>
2023-07-18 17:03:06 -0300

    aula sobre arrays

---
+++ 06 - arrays/exercicios.js
@@ -0,0 +1,8 @@
+//Crie um programa que dado um número imprime a sua tabuada
+
+const numero = 5;
+
+for (let i = 0; i <= 10; i++) {
+    const resul = numero * i;
+    console.log(numero + ' x ' + i + ' = ' + resul);    
+}
